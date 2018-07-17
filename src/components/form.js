2ef0fc8ce51f6c473bf08f4a5b82ff486bd3617e
Tom Wilson <tom@jackhq.com>
2018-07-17 13:39:01 -0400

    stage create exercise

--- src/components/form.js
+++ src/components/form.js
@@ -4,49 +4,7 @@ import { Link } from 'react-router-dom'
 import InternalComponent from '@reactions/component'
 
 export default ({ state, setState, history, match }) => (
-  <InternalComponent
-    initialState={{ color: '' }}
-    didMount={props => {
-      if (match.params.id) {
-        props.setState({ color: match.params.id })
-      }
-    }}
-  >
-    {props => (
-      <div>
-        <h1>Create/Update Color</h1>
-        <form
-          onSubmit={e => {
-            e.preventDefault()
-
-            setState(state => ({
-              colors: reject(equals(match.params.id), state.colors)
-            }))
-            setState(state => ({
-              colors: append(props.state.color, state.colors)
-            }))
-            history.push('/')
-          }}
-        >
-          <div>
-            <input
-              value={props.state.color}
-              onChange={e => {
-                props.setState({ color: e.target.value })
-              }}
-            />
-          </div>
-          <button type="submit">Create/Update Color</button>
-          <button
-            type="button"
-            onClick={() => {
-              history.push('/')
-            }}
-          >
-            cancel
-          </button>
-        </form>
-      </div>
-    )}
+  <InternalComponent initialState={{ color: '' }}>
+    {({ state: iState, setState: iSetState }) => <div>TODO: Create Color</div>}
   </InternalComponent>
 )
